41f5498b23c5228672717de8dd7626d441437c0c
Azrielski <victorngae20@gmail.com>
2024-12-15 12:09:44 +0300

    document loaded changes

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    const paragraph = document.getElementById("text");
+
+    paragraph.innerHTML = "This is really cool!"
+})
